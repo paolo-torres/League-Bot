aa4b0c27e425a846942cdab6d97d5679bf293179
Paolo Torres <paolo-torres@hotmail.com>
2017-02-26 16:40:26 -0500

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'GA5AfRgrSdC1aoPR9rzXC0AOq'
+consumer_secret = 'QKMEINKnWdNfZxTkaFeKRfZ9y2mGqPl4hXG8lrPkkqVJgNCocf'
+access_token = '829511312409501702-sOXltw3J8kZIWD6Ro3bC2iiCnPimRRv'
+access_token_secret = 'xZQEvidFhGidWUBn3YjYnCHNMU1g5pq7ffNCeMJ3DJVOs'
