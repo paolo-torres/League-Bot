fe9c8b9f39eaa8b2113020581d8f79a0401fc29a
paolotorres <paolo-torres@hotmail.com>
2017-03-31 07:48:22 -0400

    Setup .gitignore

--- reference.py
+++
@@ -1,7 +0,0 @@
-import tweepy
-from credentials import *
-
-myFile = file("C:\School\University\League Bot\config.txt", "r")
-configFile = myFile.readlines()
-key = configFile[0].split("=")[1].rstrip("\n")
-filepath = configFile[1].split("=")[1].rstrip("\n")
